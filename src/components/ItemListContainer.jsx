28117eee0bf32ee9b6570eaf21c5755d851a7142
lancierimaxi <lancierimaximilianoj@gmail.com>
2023-07-20 21:29:16 -0300

    Entregar

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ItemListContainer = ({greeting}) => {
   return (
     <div>
-        <p>
+        <p className='text-center'>
             {greeting}
         </p>
     </div>
